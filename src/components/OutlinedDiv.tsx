98426140ca63f4b2afc61a35fd77984e87180064
Danny <danny.belmonte4@gmail.com>
2024-09-06 20:14:13 -0500

    added props type to OutlinedDiv

--- src/components/OutlinedDiv.tsx
+++ src/components/OutlinedDiv.tsx
@@ -1,5 +1,14 @@
 import './css/OutlinedDiv.css'
-export default function OutlinedDiv({ children, outerDivClassName = '', innerDivClassName='' }) {
+
+
+
+type Props = {
+    children: React.ReactNode,
+    outerDivClassName: string,
+    innerDivClassName: string
+}
+
+export default function OutlinedDiv({ children, outerDivClassName = '', innerDivClassName='' }: Props) {
     return (
         <div className={`outer-div ${outerDivClassName}`}>
             <div className={`inner-div ${innerDivClassName}`}>
